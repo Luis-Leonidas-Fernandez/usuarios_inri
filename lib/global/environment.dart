f244da64c9fbae60a0459b3f1bd9741cdbed5704
Luis Leónidas Fernández <121168647+Luis-Leonidas-Fernandez@users.noreply.github.com>
2024-01-16 18:31:22 -0300

    Update environment.dart

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 
 class Environment {
   //url services
-  static String apiUrl    = Platform.isAndroid ? 'https://www.inriservice.com/api' : 'http://http://localhost:3000/api';
-  static String urlSocket = Platform.isAndroid ? 'https://www.inriservice.com/'     : 'http://http://localhost:3000';
+  static String apiUrl    = '';
+  static String urlSocket = '';
   
  
 
